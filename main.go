c9282a151042f2935a531593b7ef4f22ebaf8de3
yangjinheng <42023641@qq.com>
2021-01-15 14:27:29 +0800

    replace important token secret

--- main.go
+++ main.go
@@ -68,15 +68,15 @@ func main() {
 	// 实例化机器人
 	robot := Robot{
 		Url:    "https://oapi.dingtalk.com/robot/send",
-		Token:  "20257dae9a0ae7227755261a4033938a662bbee36bd31935fd10547ba5a27bd4",
-		Secret: "SEC560a4f53b9720d5113fe4c2a2076071b5f1f4f9acdb9625add2f984f0dd1e6e5",
+		Token:  "f2179c1b79d47d3e0e6734ad7fd337f478b2d6efa1e82392ca1739302b6fae96",
+		Secret: "SECd334bd7abf6d241693cf206530cb01c639153e61af12a2b0047ed5d0f67395b1",
 	}
 
 	// text 消息
 	res, err := robot.SendMessage(Message{
 		Msgtype: "text",
 		Text: Text{
-			Content: "text 测试",
+			Content: "你好，测试",
 		},
 	})
 	fmt.Println(res, err)
